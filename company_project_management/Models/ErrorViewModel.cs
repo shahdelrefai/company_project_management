5d7dd908247e47f434d33516765d161ab03c2d30
shahdelrefai <el.refai.shahd@gmail.com>
2023-09-23 14:48:19 +0300

    commit.

---
+++ company_project_management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace company_project_management.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
