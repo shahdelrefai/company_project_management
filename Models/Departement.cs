72dde843115b0cdfec9ec46b05c5a0be0214c0a4
Shahd ElRefai <el.refai.shahd@gmail.com>
2024-05-14 17:26:32 +0300

    Move directory

--- Models/Departement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CompanyTasksManagement.Models
-{
-    public class Departement
-    {
-        public int? Id { get; set; }
-        public string? Name { get; set; }
-        public string? Head { get; set; }
-    }
-}
