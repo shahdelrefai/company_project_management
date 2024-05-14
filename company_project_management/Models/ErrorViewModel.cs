a4c2c49263d963a023d186f468e8b6f86961dd4e
Shahd ElRefai <el.refai.shahd@gmail.com>
2024-05-14 16:51:04 +0300

    Delete old directory

--- company_project_management/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace company_project_management.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
